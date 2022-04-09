71f21a1577452fc32305e5676c72cc9bdfd48ab3
Ajey Joshi <ajey.joshi@gmail.com>
2022-04-08 23:55:09 -0400

    Added license

--- Win-10/On-Screen-Keyboard/On-Screen-Keyboard/MainWindow.xaml.cs
+++ Win-10/On-Screen-Keyboard/On-Screen-Keyboard/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿// <copyright file="MainWindow.xaml.cs" company="PlaceholderCompany">
-// Copyright (c) PlaceholderCompany. All rights reserved.
-// </copyright>
+﻿/*
+This work is licensed under the Creative Commons Attribution 4.0 International License. 
+To view a copy of this license, visit http://creativecommons.org/licenses/by/4.0/ or 
+send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+
+Creator : Ajey Joshi (ajey.joshi@gmail.com) 
+*/
 
 // ref: https://stackoverflow.com/questions/15292175/c-sharp-using-sendkey-function-to-send-a-key-to-another-application
 // REF : https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.keys?view=net-5.0
